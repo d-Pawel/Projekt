160424d2bab072586bf8b046d1169e6d679c27be
Quisy <lamprecht.mariusz@gmail.com>
2015-10-22 18:19:17 +0200

    dodane funkcje szyfrujace szyfrem xor

---
+++ encryption.hpp
@@ -0,0 +1,42 @@
+#ifndef encryption_hpp
+#define encryption_hpp
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <vector>
+
+class binary {
+	unsigned short data;
+	std::string cast();
+	std::vector<std::string> Split(const std::string& str, int splitLength);
+public:	
+	binary();
+	binary(int);
+	binary(std::string text);
+	std::string stringToBin(std::string text);
+	std::string binToString(std::string bintext);
+	//template <class T> binary(const T& d) : data(d) {}
+	friend std::ostream & operator <<(std::ostream &, binary);
+	std::string & binary::operator =(std::string&);
+};
+
+class encryption {
+private:
+	binary bn = binary();
+	std::ifstream file;
+	std::string filePath;
+	std::string key;
+	std::string cryptogram = "";
+	void generateKey(int length);
+public:
+	encryption();
+	encryption(std::string filePath) throw(std::string);
+	~encryption();
+	void encrypt();
+	void decrypt(std::string keyFilePath);
+	
+};
+
+#endif
