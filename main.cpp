9427585fe3c11a558ae8047fb256178cab0151f7
187659 <187659@edu.p.lodz.pl>
2015-11-05 18:27:16 +0100

    porzadek w nazwach

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@
 using namespace cv;
 using namespace std;
 
-string kaskadaDoRozpoznaniaTwarzy = "haarcascade_frontalface_alt.xml";
-CascadeClassifier kaskadaTwarzy;
+string faceCascadeName = "haarcascade_frontalface_alt.xml";
+CascadeClassifier faceCascade;
 
-void wykryjTwarz(Mat img);
+void detectFace(Mat img);
 
 int main(int argc, char** argv)
 {
-	if (!kaskadaTwarzy.load(kaskadaDoRozpoznaniaTwarzy)) {        //£adowanie pliku ze sprawdzeniem poprawnoci 
-		cout << "Nie znaleziono pliku " << kaskadaDoRozpoznaniaTwarzy;
+	if (!faceCascade.load(faceCascadeName)) {        //£adowanie pliku ze sprawdzeniem poprawnoci 
+		cout << "Nie znaleziono pliku " << faceCascadeName;
 		return -2;
 	}
 
@@ -29,7 +29,7 @@ int main(int argc, char** argv)
 		vector<Rect> faces;
 		Mat frame;		
 		cap >> frame; // wczytanie klatki z kamery
-		wykryjTwarz(frame);		
+		detectFace(frame);
 		if (waitKey(30) >= 0) break;
 	}
 
@@ -37,16 +37,16 @@ int main(int argc, char** argv)
 	return 0;
 }
 
-void wykryjTwarz(Mat frame) {
+void detectFace(Mat frame) {
 	vector<Rect> faces;                            //Utworzenie bufora na twarze 
-	Mat grey_frame;                                //Utworzenie bufora na obrazek w odcieniach szarosci 
-	Mat detected_face;
-	cvtColor(frame, grey_frame, CV_BGR2GRAY);                //Konwersja obrazu do odcieni szarosci 
-	kaskadaTwarzy.detectMultiScale(grey_frame, faces, 1.1, 3, 0 | CV_HAAR_SCALE_IMAGE, Size(50, 50)); //wykrycie twarzy
+	Mat greyFrame;                                //Utworzenie bufora na obrazek w odcieniach szarosci 
+	Mat detectedFace;
+	cvtColor(frame, greyFrame, CV_BGR2GRAY);                //Konwersja obrazu do odcieni szarosci 
+	faceCascade.detectMultiScale(greyFrame, faces, 1.1, 3, 0 | CV_HAAR_SCALE_IMAGE, Size(50, 50)); //wykrycie twarzy
 	for (unsigned i = 0; i < faces.size(); i++) {
-		Rect rect_face(faces[i]);    //Kwadrat okreslaj¹cy twarz ellipse( img, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 120, 12 ), 2, 2, 0 );							  
-		rectangle(frame, rect_face, Scalar(120, 5, 86), 2, 2, 0); //!!!!!tu zrobic porzadek!!!!!
-		detected_face = grey_frame(faces[i]);
+		Rect rectFace(faces[i]);    //Kwadrat okreslaj¹cy twarz ellipse( img, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 120, 12 ), 2, 2, 0 );							  
+		rectangle(frame, rectFace, Scalar(120, 5, 86), 2, 2, 0); 
+		detectedFace = greyFrame(faces[i]);
 	}	
 	imshow("Camera", frame);                         
 }
