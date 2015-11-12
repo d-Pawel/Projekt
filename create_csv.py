ce8af39e4ba878c9b27d55550c511b6527fe0b39
187659 <187659@edu.p.lodz.pl>
2015-11-12 22:08:00 +0100

    Skrypt do images/faces

--- create_csv.py
+++ create_csv.py
@@ -11,14 +11,14 @@ if __name__ == "__main__":
     BASE_PATH=os.path.dirname(os.path.abspath(__file__))
     #print (BASE_PATH)
     #name = input ("Podaj nazwe folderu ze zdjeciami: ")
-    BASE_PATH = BASE_PATH + '\images\\faces'
-    print (BASE_PATH)
+    PATH = BASE_PATH + '\images\\faces'
+    print (PATH)
     f = open('faces.csv', 'w+')
     label = 0 
-    for dirname, dirnames, filenames in os.walk(BASE_PATH):
+    for dirname, dirnames, filenames in os.walk(PATH):
         for subdirname in dirnames: 
             subject_path = os.path.join(dirname, subdirname) 
             for filename in os.listdir(subject_path): 
                 abs_path = "%s/%s" % (subject_path, filename) 
-                f.write(abs_path + ";" + str(label)+ "\n") 
+                f.write(abs_path + ";" + str(label) + ";" + subdirname + "\n") 
             label = label + 1
